3e958820799ea940398157e9695f887dde185426
Sarthak-Daga <sarthakdaga21@gmail.com>
2025-02-06 11:29:53 +0530

    cart basci route setup

--- src/app/newarrivals/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function newarrivals(){
-  return(
-    <>newArrivals</>
-  )
-}
--- src/components/navigationBar/page.tsx
+++ src/components/navigationBar/page.tsx
@@ -1,12 +1,14 @@
+import Image from "next/image";
+import shoppingCart from "@/../public/shopping-cart.png"
+
 export default function NavigationBar() {
   return (
     <nav>
       <ul className="flex flex-row justify-end gap-10 p-2 mr-5">
         <li><a href="/">Home</a></li>
         <li><a href="\shop">Shop</a></li>
-        <li><a href="\newarrivals">New Arrivals</a></li>
-        <li><a href="\collections">Collections</a></li>
-        <li><a href="\cart">Cart</a></li>
+        <li><a href="\product">Products</a></li>
+        <li><a href="\cart"><Image src={shoppingCart} alt="cart" height={22}/></a></li>
       </ul>
     </nav>
   );
